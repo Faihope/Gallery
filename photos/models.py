2d6258b15ee652313292c5dbf21383a2c7813bbb
Fai <chepngetichfaith46@gmail.com>
2021-07-02 19:05:36 +0300

    Create admin dashboard and update migrations

--- photos/models.py
+++ photos/models.py
@@ -4,23 +4,23 @@ from django.db.models.base import Model
 # Create your models here.
 
 class Category(models.Model):
-    cat_name=models.CharField(max_length=100,blank=False )
+    category_name=models.CharField(max_length=100,blank=True )
     
 
     def __str__(self):
-        return self.cat_name
+        return self.category_name
 
 class Location(models.Model):
-    loc_name=models.CharField(max_length=50,blank=False )
+    location_name=models.CharField(max_length=50,blank=False )
     
 
     def __str__(self):
-        return self.loc_name
+        return self.location_name
 
 
 
 class Image(models.Model):
-    Image=models.ImageField(null=True,blank=False)
+    image=models.ImageField(null=True,blank=False)
     name=models.CharField(max_length=100)
     description=models.CharField(max_length=4000)
     category=models.ForeignKey(Category,on_delete=models.CASCADE)
--- photos/urls.py
+++ photos/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns=[
@@ -8,4 +10,5 @@ urlpatterns=[
     path('photo/<str:pk>',views.viewPhoto,name='photo'),
     url('add',views.addPhoto,name='add')
 ]
-
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
