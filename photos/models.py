2f075751c8cd0e43ea18546ecadd3914061a22ff
Fai <chepngetichfaith46@gmail.com>
2021-07-02 17:32:38 +0300

    Add image,category and location models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+
+class Category(models.Model):
+    cat_name=models.CharField(max_length=100,blank=False )
+    
+
+    def __str__(self):
+        return self.cat_name
+
+class Location(models.Model):
+    loc_name=models.CharField(max_length=50,blank=False )
+    
+
+    def __str__(self):
+        return self.loc_name
+
+
+
+class Image(models.Model):
+    Image=models.ImageField(null=True,blank=False)
+    name=models.CharField(max_length=100)
+    description=models.CharField(max_length=4000)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    location=models.ForeignKey(Location,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
+
+
+
+
+
