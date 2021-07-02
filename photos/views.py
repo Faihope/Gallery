1126ed5bb9f25b93d0f9a3ed5017ca621d4f2e44
Fai <chepngetichfaith46@gmail.com>
2021-07-02 19:21:56 +0300

    Query the database to display the image categories

--- photos/views.py
+++ photos/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from .models import Category,Image
 
 # Create your views here.
 def gallery(request):
-    return render(request,'gallery.html')
+    categories= Category.objects.all()
+    context= {'categories':categories}
+    return render(request,'gallery.html',context)
 
 def viewPhoto(request,pk=int):
     return render(request,'photo.html')
