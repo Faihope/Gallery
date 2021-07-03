0fcb3de82b9b916877715b1f56c5806fe0dd0ebc
Fai <chepngetichfaith46@gmail.com>
2021-07-03 14:56:01 +0300

    Add the logics to enable upload new photo

--- photos/models.py
+++ photos/models.py
@@ -4,14 +4,14 @@ from django.db.models.base import Model
 # Create your models here.
 
 class Category(models.Model):
-    category_name=models.CharField(max_length=100,blank=True )
+    category_name=models.CharField(max_length=100,blank=True,null=True )
     
 
     def __str__(self):
         return self.category_name
 
 class Location(models.Model):
-    location_name=models.CharField(max_length=50,blank=False )
+    location_name=models.CharField(max_length=50,blank=False ,null=True)
     
 
     def __str__(self):
--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Category,Image,Location
+
 
 # Create your views here.
 def gallery(request):
@@ -15,6 +16,25 @@ def viewPhoto(request,pk=int):
 
 def addPhoto(request):
     categories= Category.objects.all()
+
+    if request.method == 'POST':
+        data=request.POST
+        image=request.FILES.get('image')
+
+        if data['category']!='none':
+            category=Category.objects.get(id=data['category'])
+
+        elif data['category_new']!='':
+            category, created=Category.objects.get_or_create(name=data['category_new'])
+        else:
+            category=None
+
+        photo = Image.objects.create(
+            category=category,
+            description=data['description'],
+            image=image
+        )
+        return redirect('gallery')
 
     context= {'categories':categories}
     return render(request,'add.html',context)
